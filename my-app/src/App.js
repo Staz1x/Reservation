496b08eb85d0feea33d69b41ffbb45f0071f7235
Staz1x <fogel.stazix@gmail.com>
2024-02-16 15:39:03 +0100

    Updated calendar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,30 @@
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
+import {useState} from "react";
+
+const minDate = Date.now();
+const today = new Date(minDate);
+const maxDate = today;
+
+console.log(today.toDateString());
+console.log(maxDate);
 
 function App() {
+    const [value, setValue] = useState(new Date());
+
+    function onChange(nextValue) {
+        setValue(nextValue);
+    }
+
   return (
-    <div> <p> Hello World </p>
-      <Calendar />
+    <div> <p className="header"> Select date </p>
+      <Calendar
+          onChange={onChange}
+          value={value}
+          minDate={new Date(minDate)}
+          //maxDate={new Date(maxDate)}
+          selectRange={true} />
     </div>
   );
 }
