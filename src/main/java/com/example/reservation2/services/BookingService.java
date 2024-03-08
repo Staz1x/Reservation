b2b0b7c67e3c94521626e4225732e53f54f95df9
Martin Cruces <martin76e@yahoo.com>
2024-03-08 15:54:37 +0100

    Added methods in booking services: createBooking, getAllBookings, getBookingByID, getBookingsByUserId, deleteBookingById.

--- src/main/java/com/example/reservation2/services/BookingService.java
+++ src/main/java/com/example/reservation2/services/BookingService.java
@@ -1,4 +1,17 @@
 package com.example.reservation2.services;
 
+import com.example.reservation2.models.Booking;
+
+import java.util.List;
+
 public interface BookingService{
+    Booking createBooking(Booking booking);
+
+    List<Booking> getAllBookings();
+
+    Booking getBookingById(Long id);
+
+    List<Booking> getBookingsByUserId(Long userId);
+
+    void deleteBookingById(Long id);
 }
--- src/main/java/com/example/reservation2/services/BookingServiceImpl.java
+++ src/main/java/com/example/reservation2/services/BookingServiceImpl.java
@@ -1,4 +1,32 @@
 package com.example.reservation2.services;
 
+import com.example.reservation2.models.Booking;
+
+import java.util.List;
+
 public class BookingServiceImpl implements BookingService{
+    @Override
+    public Booking createBooking(Booking booking) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getAllBookings() {
+        return null;
+    }
+
+    @Override
+    public Booking getBookingById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingsByUserId(Long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteBookingById(Long id) {
+
+    }
 }
