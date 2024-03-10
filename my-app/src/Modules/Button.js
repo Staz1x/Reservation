5e31ba7e0435cd9154be433e8814c17de425502f
Staz1x <fogel.stazix@gmail.com>
2024-03-10 11:45:28 +0100

    Moved all Css to a folder, Set up basic login with validation. TODO Authentication

--- my-app/src/Modules/Button.js
+++ my-app/src/Modules/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Button.css'
+import './Css/Button.css'
 
 const Button = ({ onClick, text }) => {
     return <button className="button" onClick={onClick}>{text}</button>;
--- my-app/src/Modules/LoginPage.js
+++ my-app/src/Modules/LoginPage.js
@@ -1,27 +1,78 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import Button from "./Button";
+import './Css/LoginPage.css'
 import {useNavigate} from "react-router-dom";
 
-function LoginPage() {
+const LoginPage = (props) => {
+    const [user, setUser] = useState('')
+    const [password, setPassword] = useState('')
+    const [userError, setUserError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+
     const navigate = useNavigate();
 
     const handleButtonClick = () => {
+
+        setUserError('')
+        setPasswordError('')
+
+        // Check if the user has entered both fields correctly
+        if ('' === user) {
+            setUserError('Please enter your username')
+            return
+        }
+
+        if (/\d$/.test(user)) {
+            setUserError("Can't use numbers in username")
+            return
+        }
+
+        if ('' === password) {
+            setPasswordError('Please enter a password')
+            return
+        }
+
+        if (password.length < 3) {
+            setPasswordError('The password must be 4 characters or longer')
+            return
+        }
+
         navigate({
             pathname: '/calendar',
         });
     };
 
     return (
-
-        <div>
-            <p>Hello World</p>
-            <Button
-                onClick={handleButtonClick}
-                text="NEXT"
-            />
+        <div className={'mainContainer'}>
+            <div className={'titleContainer'}>
+                <div>Login</div>
+            </div>
+            <br/>
+            <div className={'inputContainer'}>
+                <input
+                    value={user}
+                    placeholder="Enter your username"
+                    onChange={(ev) => setUser(ev.target.value)}
+                    className={'inputBox'}
+                />
+                <label className="errorLabel">{userError}</label>
+            </div>
+            <br/>
+            <div className={'inputContainer'}>
+                <input
+                    value={password}
+                    placeholder="Enter your password"
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    className={'inputBox'}
+                />
+                <label className="errorLabel">{passwordError}</label>
+            </div>
+            <br/>
+            <div className={'inputContainer'}>
+                <Button onClick={handleButtonClick} text="Log in"/>
+            </div>
         </div>
-
     );
-
 }
+
 export default LoginPage;
