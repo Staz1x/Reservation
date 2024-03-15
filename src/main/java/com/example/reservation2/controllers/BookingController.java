21d4a5c159eea386be13a8d9e92bbb51acda82e1
Martin Cruces <martin76e@yahoo.com>
2024-03-15 17:47:53 +0100

    added mappings for BookingController, to find all bookings for a user (getBookingByUserId).

--- src/main/java/com/example/reservation2/controllers/BookingController.java
+++ src/main/java/com/example/reservation2/controllers/BookingController.java
@@ -1,17 +1,59 @@
 package com.example.reservation2.controllers;
 
+import com.example.reservation2.models.Booking;
+import com.example.reservation2.models.User;
 import com.example.reservation2.services.BookingService;
+import com.example.reservation2.services.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/bookings")
 public class BookingController {
 
     private BookingService bookingService;
 
+    private UserService userService;
 
 
-    public BookingController(BookingService bookingService){
+
+    public BookingController(BookingService bookingService, UserService userService){
         this.bookingService = bookingService;
-
+        this.userService = userService;
     }
+
+/*
+    @GetMapping("/user/{userId}")
+    public List<Booking> getBookingsByUserId(@PathVariable Long userId) {
+        User user = userService.getUserById(userId);
+        if (user == null) {
+            //Create Exception if needed.
+            throw new UserNotFoundException("User not found with id: " + userId);
+        }
+        return bookingService.getBookingsByUser(user);
+    }
+*/
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Booking>> getBookingsByUserId(@PathVariable Long userId) {
+        User user = userService.getUserById(userId);
+        if (user == null) {
+            // User not found, return HTTP 404 Not Found
+            return ResponseEntity.notFound().build();
+        }
+        List<Booking> bookings = bookingService.getBookingsByUser(user);
+        if (bookings.isEmpty()) {
+            // No bookings found, return HTTP 204 No Content
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(bookings);
+    }
+
+
 
 }
