3f7c97a84eeb6d6ab232fe9fc101d0b103f0ea3f
Martin Cruces <martin76e@yahoo.com>
2024-04-25 09:56:00 +0200

    refactoring

--- src/main/java/com/example/reservation2/repositories/UserRoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.reservation2.repositories;
-
-import com.example.reservation2.models.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-}
