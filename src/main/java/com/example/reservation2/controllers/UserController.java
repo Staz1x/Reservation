e04138de2bb4db3cc2ad6da0fb4cf25459d798a1
Martin Cruces <martin76e@yahoo.com>
2024-05-07 09:58:01 +0200

    Update UserController class.

--- src/main/java/com/example/reservation2/controllers/UserController.java
+++ src/main/java/com/example/reservation2/controllers/UserController.java
@@ -1,7 +1,14 @@
 package com.example.reservation2.controllers;
 
+import com.example.reservation2.services.UserService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
 }
