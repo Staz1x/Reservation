27218cb64ccc7da661abcd57fc0cb759fb2bafc7
Martin Cruces <martin76e@yahoo.com>
2024-05-07 13:07:39 +0200

    Added UserServiceImplTest

---
+++ src/test/java/com/example/reservation2/services/UserServiceImplTest.java
@@ -0,0 +1,33 @@
+package com.example.reservation2.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceImplTest {
+
+
+    @BeforeEach
+    public void setup(){
+
+    }
+
+    @Test
+    void getUserById() {
+
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
